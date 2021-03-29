2d09e1f36d134e6998d68fdfa91b6e8bb546d327
Cristina Plesa <cristinaplesa8@gmail.com>
2021-03-29 16:29:26 -0700

    Add function to display array as a list.
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
   const numbers = ["Ace of ", "2 of ", "3 of ", "4 of ", "5 of ", "6 of ", "7 of ", "8 of ", "9 of ", "10 of ","Jack of ", "Queen of ", "King of "];
-  const suits = ["Clubs", "Spades", "Diamonds", "Hearts"];
+  const suits = ["Clubs ", "Spades ", "Diamonds ", "Hearts "];
   suits.forEach(function(suit) {
     numbers.forEach(function(number) {
     const cardName = number.concat(suit);
-
-    $(".display").append(cardName);
+    $(".display").append("<li>" + cardName);
   });
 });
 
