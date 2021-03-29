13475399884aa2bdf36b9381c68b68706e8c11e4
Cristina Plesa <cristinaplesa8@gmail.com>
2021-03-29 15:28:11 -0700

    Add array argument and forEach function in js.
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  const numbers = ["Ace of ", "2 of ", "3 of ", "4 of ", "5 of ", "6 of ", "7 of ", "8 of ", "9 of ", "10 of ","Jack of ", "Queen of ", "King of "];
+  const suits = ["Clubs", "Spades", "Diamonds", "Hearts"];
+  suits.forEach(function(suit) {
+    numbers.forEach(function(number) {
+    const cardName = number.concat(suit);
+    console.log(cardName);
+  });
+});
+
+});
